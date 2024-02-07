bd43d8b1c2e124d8d035165152d72fcda9a5f340
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-02-06 19:29:23 -0500

    Class Exercise Series 2, coding

--- ExerciseSeries2_MariaMelendez/Program.cs
+++ ExerciseSeries2_MariaMelendez/Program.cs
@@ -10,6 +10,38 @@ namespace ExerciseSeries2_MariaMelendez
     {
         static void Main(string[] args)
         {
+            // Write a C# program that reads a number and indicates wheter it is positive, negative, zero, odd or even. 
+            string positiveNegative = "Positive";
+            string evenOdd = "Even";
+            Console.WriteLine("Please enter a whole number: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (num < 0)
+            {
+                positiveNegative = "Negative";
+
+                if (num % 2 != 0) 
+                {
+                    evenOdd = "Odd";
+                }
+            }
+            else if (num > 0)
+            {
+                if (num % 2 != 0)
+                {
+                    evenOdd = "Odd";
+                }
+            }
+            else
+            {
+                positiveNegative = "Zero";
+            }
+
+            Console.WriteLine("The number is {0} and it is {1}", positiveNegative, evenOdd);
+
+
+
+
         }
     }
 }
